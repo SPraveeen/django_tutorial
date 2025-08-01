df5500a360321c0a62f3b0ea904879ad950d1081
SPraveeen <praveenji8498@gmail.com>
2025-08-01 12:52:38 +0530

    1:20:00

---
+++ Inventory/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home/',HomePage),
+    path('about/',AboutPage),
+    path('contact/',ContactPage),
+    path('service/',ServicePage),
+]
--- Inventory/views.py
+++ Inventory/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def HomePage(request):
+    data={
+        "name":"praveen",
+        "role":"manager",
+        "numbers":[1,2,3,4,5],
+        "marks":{
+            "tamil":100,
+            "english":90
+        }
+    }
+    return render(request,'home.html',data)
+
+def AboutPage(request):
+    return render(request,'about.html')
+
+def ContactPage(request):
+    return render(request,'contact.html')
+
+def ServicePage(request):
+    return render(request,'service.html')
