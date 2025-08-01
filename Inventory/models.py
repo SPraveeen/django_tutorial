d258d99d6539b310c7406e4475461c5cf1a86118
SPraveeen <praveenji8498@gmail.com>
2025-08-01 17:14:41 +0530

    model created 1:44:00

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name= models.CharField(max_length=200,null=True)
+    product_code=models.CharField(max_length=200,null=True)
+    product_price=models.FloatField(default=0)
+    gst=models.IntegerField(default=0)
+    food_product=models.BooleanField(default=False)
