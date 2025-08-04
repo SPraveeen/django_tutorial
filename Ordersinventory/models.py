813f662432adcd3be64edcd068bf6724fe89f86e
SPraveeen <praveenji8498@gmail.com>
2025-08-04 11:54:18 +0530

    2:50:00

--- Ordersinventory/models.py
+++ Ordersinventory/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Customer(models.Model):
+    customer_name=models.CharField(max_length=200,null=True)
+    customer_since=models.DateField(null=True)
 
+    def __str__(self):
+        return self.customer_name
--- Ordersinventory/views.py
+++ Ordersinventory/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+def CustomerList(request):
+    return render(request,'customers.html')
+
+def CustomerAdd(request):
+    return render(request,'customers_add.html')
+
+def CustomerDelete(request,id):
+    selected_customer=Customer.objects.get(id=id)
+    selected_customer.delete()
+
+def CustomerUpdate(request,id):
+    selected_customer=Customer.objects.get(id=id)
