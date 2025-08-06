951c5c4b8f0b5e15c7a734972e2320bc46bbd4a5
SPraveeen <praveenji8498@gmail.com>
2025-08-06 08:22:05 +0530

    created menu

--- Inventory/admin.py
+++ Inventory/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from Ordersinventory.models import *
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Customer)
